20f71c2d0e29ab6ed1d0b48f4ee9b6e109dbaf73
costaleonardo <costaleonardo97@gmail.com>
2018-05-09 15:23:31 -0400

    Add parsing for payloads.

--- index.js
+++ index.js
@@ -9,26 +9,42 @@
 
 // Dependencies
 const http = require('http'),
-      url  = require('url');
+      url  = require('url')
+      StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should responde to all requests with a string
 const server = http.createServer((req, res) => {
   // Get the URL and parse it
   const parsedUrl = url.parse(req.url, true);
+
   // Get the path
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
   // Get the query string as an object
   const queryStringObject = parsedUrl.query;
+
   // Get the HTTP method
   const method = req.method.toLowerCase();
+
   // Get the headers as an object
   const headers = req.headers;
 
-  // Send the response
-  res.end('Hello World\n');
-  // Log the request path
-  console.log(`Request recieved on path: ${trimmedPath} with ${method} method with these string parameters.`, queryStringObject);
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', data => {
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    // Send the response
+    res.end('Hello World\n');
+
+    // Log the request path
+    console.log('Request received with this payload: ', buffer);
+  });
 });
 
 // Start the server, and have it listen on port 3000
