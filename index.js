e4c1bc24db4a5b638a1245a154cffda465f356c8
costaleonardo <costaleonardo97@gmail.com>
2018-05-09 14:31:39 -0400

    Add parsin to request paths.

--- index.js
+++ index.js
@@ -8,11 +8,21 @@
   **/
 
 // Dependencies
-const http = require('http');
+const http = require('http'),
+      url  = require('url');
 
 // The server should responde to all requests with a string
 const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send the response
   res.end('Hello World\n');
+  // Log the request path
+  console.log(`Request Recieved on path: ${trimmedPath}`);
 });
 
 // Start the server, and have it listen on port 3000
